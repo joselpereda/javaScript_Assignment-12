147dcef18830304c15606cdf02594916e117e61c
Jose Pereda <joselpereda@hotmail.com>
2023-05-17 15:17:40 -0700

    updated player.js

--- js/player.js
+++ js/player.js
@@ -1,19 +1,22 @@
-class Jukebox {
+class Player {
     constructor(albums) {
-        this.albums = []
+        this.albums = [];
     }
     addAlbum = function(album) {
-        this.albums.push(album)
+        this.albums.push(album);
     }
     favoriteAlbum = function() {
-        let max = -1, fav
+        let max = -1, fav;
         for (let i = 0; i < this.albums.length; i++) {
             if (this.albums[i].played > max) {
-                max = this.albums[i].played
-                fav = this.albums[i]
+                max = this.albums[i].played;
+                fav = this.albums[i];
             }
         }
-        return fav.display()
+        return fav.display();
+    }
+    returnAlbum = function(num) {
+        return this.albums[num];
     }
 }
 
@@ -31,21 +34,31 @@ class Album {
     }
 }
 
-var jbox = new Jukebox()
-const album1 = new Album('Operation Ivy', 'Energy')
-const album2 = new Album('Blink 182', 'Dude Ranch')
-const album3 = new Album('New Found Glory', 'Sticks and Stones')
+window.addEventListener('load', () => {  
+    const player = new Player();
+    const albumList = [
+        ['Operation Ivy', 'Energy'],
+        ['New Found Glory', 'Sticks and Stones'],
+        ['Phil Collins', '...Hits'],
+        ['Journey', 'Escape'],
+        ['Prince', 'Dirty Mind']];
 
-jbox.addAlbum(album1)
-jbox.addAlbum(album2)
-jbox.addAlbum(album3)
+let albums          = document.getElementById('albums');
+let play            = document.getElementById('play');
+let showFav         = document.getElementById('favorite');
+let displayFav      = document.getElementById('favoriteAlbum');
 
-album1.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album3.play()
+for (let i = 0; i < albumList.length; i += 1) {
+    player.addAlbum(new Album(albumList[i][0], albumList[i][1]));
+    albums.add(new Option(albumList[i][0] + ' : ' + albumList[i][1], i));
+}
 
-console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+play.addEventListener('click', () => {
+    player.returnAlbum(albums.value).play();
+    console.log(player.returnAlbum(albums.value).display());
+});
+
+showFav.addEventListener('click', () => {
+    displayFav.innerHTML = player.favoriteAlbum();
+});
+});
